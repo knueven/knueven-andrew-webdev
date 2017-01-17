89939232803a02d672292b1bced2193582190430
Andrew Knueven <knuevenam@gmail.com>
2017-01-17 17:10:19 -0500

    right place for mlab URI

--- server.js
+++ server.js
@@ -5,10 +5,6 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-var connectionString = 'mongodb://127.0.0.1:27017/test';
-if(process.env.MONGODB_URI){
-    connectionString = process.env.MONGODB_URI
- }
 // configure a public directory to host static content
 app.use(express.static(__dirname + '/public'));
 
